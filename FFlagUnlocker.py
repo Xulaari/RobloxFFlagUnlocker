3cf513dc0ef6a185a21d62ddc2f290f0b24b0495
Xula <74150275+Xulaari@users.noreply.github.com>
2023-05-01 21:49:26 +0100

    Update FFlagUnlocker.py
    Bloxstrap support added

--- FFlagUnlocker.py
+++ FFlagUnlocker.py
@@ -15,25 +15,36 @@ if addto == "":
 else:
     fps = int(numpy.ceil(refreshrate) + float(addto))
 
-settings = {
+robloxsettings = {
     "DFIntTaskSchedulerTargetFps": fps
+}
+
+bloxstrapsettings = {
+    "DFIntTaskSchedulerTargetFps": fps,
+    "FFlagDebugGraphicsPreferD3D11": "True"
 }
 
 username = os.getlogin()
 
 try:
-    rbxv = requests.get("https://setup.rbxcdn.com/version")
+    robloxversion = requests.get("https://setup.rbxcdn.com/version")
 except Exception as k:
     print("There was an error requesting roblox's version.")
 
 
-rbxpth = f"C:\\Users\\{username}\\Appdata\\Local\\Roblox\\Versions\\{rbxv.text}\\ClientSettings"
+robloxpath = f"C:\\Users\\{username}\\Appdata\\Local\\Roblox\\Versions\\{robloxversion.text}\\ClientSettings"
+bloxstrappath = f"C:\\Users\\{username}\\AppData\\Local\\Bloxstrap\\Modifications\\ClientSettings"
 
-print("Got roblox version at " + rbxv.text[8:] + ". Adding FFlag.")
+print("Got roblox version at " + robloxversion.text[8:] + ". Adding FFlag.")
 
-if os.path.exists(rbxpth):
+if os.path.exists(robloxpath):
     try:
         print("ClientSettings Folder already found, Rewriting.")
+    except Exception as q:
+        print("An error occurred. See " + q)
+if os.path.exists(robloxpath) and os.path.exists(bloxstrappath):
+    try:
+        print("Roblox and Bloxstrap Clientsettings folder found, Rewriting.")
     except Exception as q:
         print("An error occurred. See " + q)
 
@@ -43,19 +54,38 @@ def killroblox():
             proc.kill()
             break
 
-def writefile():
+def robloxwritefile():
     try:
       killroblox()
-      os.makedirs(rbxpth, exist_ok=True)
-      file_path = os.path.join(rbxpth, "ClientAppSettings.json")
+      os.makedirs(robloxpath, exist_ok=True)
+      file_path = os.path.join(robloxpath, "ClientAppSettings.json")
       with open(file_path, "w") as f:
-         json.dump(settings, f, indent=4)
+         json.dump(robloxsettings, f, indent=4)
     except Exception as k:
-      print("An error has happened | " + k)
+      print("An error occured. See:  " + k)
 
-if refreshrate >= 60:
-    writefile()
-else:
-    print("FFlag not added. Refresh Rate detected as {refreshrate}. ")
+def bloxstrapwritefile():
+    try:
+      killroblox()
+      os.makedirs(bloxstrappath, exist_ok=True)
+      file_path = os.path.join(bloxstrappath, "ClientAppSettings.json")
+      with open(file_path, "w") as f:
+         json.dump(bloxstrapsettings, f, indent=4)
+    except Exception as n:
+        print("An error occured. See: " + n)
+
+if refreshrate >= 60 and os.path.exists(robloxpath):
+    robloxwritefile()
+elif refreshrate >= 60 and os.path.exists(bloxstrappath):
+    bloxstrapwritefile()
+if refreshrate >= 60 and os.path.exists(robloxpath) and os.path.exists(bloxstrappath):
+    robloxwritefile()
+    bloxstrapwritefile()
+if refreshrate >= 60 and not os.path.exists(robloxpath):
+    os.mkdir(os.path.join(f"C:\\Users\\{username}\\Appdata\\Local\\Roblox\\Versions\\{robloxversion.text}"))
+    robloxwritefile()
+if refreshrate >= 60 and not os.path.exists(bloxstrappath):
+    os.mkdir(os.path.join(f"C:\\Users\\{username}\\AppData\\Local\\Bloxstrap\\Modifications\\ClientSettings"))
+    bloxstrapwritefile()
 
 input("FFlag added. \x1b[39mPress \x1b[32m[Enter]\x1b[39m to exit. ")
