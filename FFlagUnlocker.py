e53dcb87b94b4630c9ab04270402752879a62013
Xula <74150275+Xulaari@users.noreply.github.com>
2023-05-03 22:06:15 +0100

    M1 Version Release

--- FFlagUnlocker.py
+++ FFlagUnlocker.py
@@ -1,91 +1,43 @@
 import requests
 import os
-import json
-import numpy
 import win32api
-import psutil
-
-print("Software by \x1b[34mxula\x1b[34m!")
-addto = input("\x1b[39mPress \x1b[32m[Enter]\x1b[39m to use your monitors HZ or enter what to add on or takeaway : ")
+import time
 
 refreshrate = win32api.EnumDisplaySettings(None, 0).DisplayFrequency
 
-if addto == "":
-    fps = int(numpy.ceil(refreshrate) + 1)
-else:
-    fps = int(numpy.ceil(refreshrate) + float(addto))
+print("\x1b[36mFFlagUnlocker\x1b[39m | Coded in python and built with love by \x1b[31mxula!\x1b[39m\n")
 
-robloxsettings = {
-    "DFIntTaskSchedulerTargetFps": fps
-}
+robloxversion = requests.get("https://setup.rbxcdn.com/version").text
+robloxversionreadable = requests.get("https://setup.rbxcdn.com/version").text.replace('version-', '')
 
-bloxstrapsettings = {
-    "DFIntTaskSchedulerTargetFps": fps,
-    "FFlagDebugGraphicsPreferD3D11": "True"
-}
+print(f"Roblox version gateway at: {robloxversionreadable}")
+takeinnoyes = input(f"{os.getlogin()}, Press any key \x1b[36m[Enter]\x1b[39m to make FPS Cap your monitors HZ or \x1b[36m:q\x1b[39m To set a custom one: ")
 
-username = os.getlogin()
+if not os.path.exists(f"C:\\Users\\{os.getlogin()}\\AppData\\Local\\Roblox\\Versions\\{robloxversion}\ClientSettings"):
+    os.mkdir(f"C:\\Users\\{os.getlogin()}\\AppData\\Local\\Roblox\\Versions\\{robloxversion}\ClientSettings")
 
 try:
-    robloxversion = requests.get("https://setup.rbxcdn.com/version")
-except Exception as k:
-    print("There was an error requesting roblox's version, See: " + k)
+    if takeinnoyes == ":q":
+        os.system('cls')
+        print("\x1b[39mFFlagUnlocker. Built with love by \x1b[31mxula!\x1b[39m\n")
+        fpscapifcustom = int(input("Detected \x1b[36m:q\x1b[39m. Enter what FPS Cap to set: "))
+        print("Applying custom settings.")
+        with open(f"C:\\Users\\{os.getlogin()}\\Appdata\\Local\\Roblox\\Versions\\{robloxversion}\\ClientSettings\\ClientAppSettings.json", 'w') as nv:
+            nv.write(f"""{{
+    "DFIntTaskSchedulerTargetFps": {fpscapifcustom}
+}}""")
+    elif refreshrate <= 60:
+        print(f"HZ {refreshrate}. Not applied. Closing.")
+        time.sleep(3)
+        exit()
+    else:
+        print("Applying default settings.")
+        with open(f"C:\\Users\\{os.getlogin()}\\Appdata\\Local\\Roblox\\Versions\\{robloxversion}\\ClientSettings\\ClientAppSettings.json", 'w') as n:
+            n.write(f"""{{
+    "DFIntTaskSchedulerTargetFps": {refreshrate}
+}}""")
+except FileNotFoundError or NotADirectoryError:
+    print("Your roblox is out of date from the newest version.")
 
-
-robloxpath = f"C:\\Users\\{username}\\Appdata\\Local\\Roblox\\Versions\\{robloxversion.text}\\ClientSettings"
-bloxstrappath = f"C:\\Users\\{username}\\AppData\\Local\\Bloxstrap\\Modifications\\ClientSettings"
-
-print("Got roblox version at " + robloxversion.text[8:] + ". Adding FFlag.")
-
-if os.path.exists(robloxpath) and not os.path.exists(bloxstrappath):
-    try:
-        print("ClientSettings Folder already found, Rewriting.")
-    except Exception as q:
-        print("An error occurred. See " + q)
-if os.path.exists(robloxpath) and os.path.exists(bloxstrappath):
-    try:
-        print("Roblox and Bloxstrap Clientsettings folder found, Rewriting.")
-    except Exception as q:
-        print("An error occurred. See " + q)
-
-def killroblox():
-    for proc in psutil.process_iter():
-        if proc.name() == "RobloxPlayerBeta.exe":
-            proc.kill()
-            break
-
-def robloxwritefile():
-    try:
-      killroblox()
-      os.makedirs(robloxpath, exist_ok=True)
-      file_path = os.path.join(robloxpath, "ClientAppSettings.json")
-      with open(file_path, "w") as f:
-         json.dump(robloxsettings, f, indent=4)
-    except Exception as v:
-      print("An error occured. See:  " + v)
-
-def bloxstrapwritefile():
-    try:
-      killroblox()
-      os.makedirs(bloxstrappath, exist_ok=True)
-      file_path = os.path.join(bloxstrappath, "ClientAppSettings.json")
-      with open(file_path, "w") as f:
-         json.dump(bloxstrapsettings, f, indent=4)
-    except Exception as n:
-        print("An error occured. See: " + n)
-
-if refreshrate >= 60 and os.path.exists(robloxpath):
-    robloxwritefile()
-elif refreshrate >= 60 and os.path.exists(bloxstrappath):
-    bloxstrapwritefile()
-if refreshrate >= 60 and os.path.exists(robloxpath) and os.path.exists(bloxstrappath):
-    robloxwritefile()
-    bloxstrapwritefile()
-if refreshrate >= 60 and not os.path.exists(robloxpath):
-    os.mkdir(os.path.join(f"C:\\Users\\{username}\\Appdata\\Local\\Roblox\\Versions\\{robloxversion.text}"))
-    robloxwritefile()
-if refreshrate >= 60 and not os.path.exists(bloxstrappath):
-    os.mkdir(os.path.join(f"C:\\Users\\{username}\\AppData\\Local\\Bloxstrap\\Modifications\\ClientSettings"))
-    bloxstrapwritefile()
-
-input("FFlag added. \x1b[39mPress \x1b[32m[Enter]\x1b[39m to exit. ")
+input("Press any key to exit. Thank you for using the \x1b[36mM1\x1b[39m version of \x1b[36mFFlagUnlocker!\x1b[39m")
+exit()
